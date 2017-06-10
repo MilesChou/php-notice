640c534fafc16f7ad42fab9f8baedc6d552a007f
MilesChou <jangconan@gmail.com>
2017-06-10 18:32:14 +0800

    Add int checker test

---
+++ tests/CheckIntTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Notice;
+
+class CheckIntTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldReturnFalseWhenCallIsIntWithStringTypeInteger()
+    {
+        $this->assertFalse(is_int('100'));
+
+        $this->assertTrue(is_numeric('100'));
+    }
+}
