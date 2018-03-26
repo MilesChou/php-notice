888e5fceb43828a2f8c8b7b3cb21201cd3b859f7
MilesChou <jangconan@gmail.com>
2018-03-27 00:34:08 +0800

    Add notices

---
+++ tests/CastNoticeTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Notice;
+
+class CastNoticeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function autoCastBehavior()
+    {
+        $this->assertTrue(false == []);
+        $this->assertTrue([] == false);
+
+        $this->assertFalse(false === []);
+        $this->assertFalse([] === false);
+
+        $this->assertTrue(0 == 'whatever');
+        $this->assertTrue('whatever' == 0);
+
+        $this->assertFalse(0 === 'whatever');
+        $this->assertFalse('whatever' === 0);
+    }
+}
