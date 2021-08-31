ababbc5a7f99a31ab6124c388abcd8b724fc0a2c
miles.chou <miles.chou@104.com.tw>
2021-08-31 10:55:48 +0800

    Add empty test

--- tests/IssetEmptyTest.php
+++ tests/IssetEmptyTest.php
@@ -12,7 +12,7 @@ use stdClass;
  */
 class IssetEmptyTest extends TestCase
 {
-    public function issetTrueArrayCase(): iterable
+    public function issetTrueCase(): iterable
     {
         yield [['foo' => '']];
         yield [['foo' => []]];
@@ -31,7 +31,7 @@ class IssetEmptyTest extends TestCase
         yield [['foo' => new stdClass()]];
     }
 
-    public function issetFalseArrayCase(): iterable
+    public function issetFalseCase(): iterable
     {
         yield [[]];
         yield [['foo' => null]];
@@ -39,9 +39,9 @@ class IssetEmptyTest extends TestCase
 
     /**
      * @test
-     * @dataProvider issetTrueArrayCase
+     * @dataProvider issetTrueCase
      */
-    public function shouldReturnTrueWhenIssetTrueArrayCaseUsingIsset($arr)
+    public function shouldReturnTrueWhenIssetTrueArrayCaseUsingIsset($arr): void
     {
         $actual = isset($arr['foo']);
 
@@ -50,9 +50,9 @@ class IssetEmptyTest extends TestCase
 
     /**
      * @test
-     * @dataProvider issetTrueArrayCase
+     * @dataProvider issetTrueCase
      */
-    public function shouldReturnTrueWhenIssetTrueArrayCaseUsingIssetOrNotEmpty($arr)
+    public function shouldReturnTrueWhenIssetTrueArrayCaseUsingIssetOrNotEmpty($arr): void
     {
         $actual = isset($arr['foo']) || !empty($arr['foo']);
 
@@ -61,9 +61,9 @@ class IssetEmptyTest extends TestCase
 
     /**
      * @test
-     * @dataProvider issetFalseArrayCase
+     * @dataProvider issetFalseCase
      */
-    public function shouldReturnFalseWhenIssetFalseArrayCaseUsingIsset($arr)
+    public function shouldReturnFalseWhenIssetFalseArrayCaseUsingIsset($arr): void
     {
         $actual = isset($arr['foo']);
 
@@ -72,11 +72,92 @@ class IssetEmptyTest extends TestCase
 
     /**
      * @test
-     * @dataProvider issetFalseArrayCase
+     * @dataProvider issetFalseCase
      */
-    public function shouldReturnFalseWhenIssetFalseArrayCaseUsingIssetOrNotEmpty($arr)
+    public function shouldReturnFalseWhenIssetFalseArrayCaseUsingIssetOrNotEmpty($arr): void
     {
         $actual = isset($arr['foo']) || !empty($arr['foo']);
+
+        $this->assertFalse($actual);
+    }
+
+    public function emptyFalseCase(): iterable
+    {
+        // Use !empty will return true cases
+        yield [['foo' => ['a', 'b']]];
+        yield [['foo' => true]];
+        yield [['foo' => 1]];
+        yield [['foo' => -1]];
+        yield [['foo' => '1']];
+        yield [['foo' => '-1']];
+        yield [['foo' => 'str']];
+        yield [['foo' => 'true']];
+        yield [['foo' => 'false']];
+        yield [['foo' => new stdClass()]];
+    }
+
+    public function emptyTrueCase(): iterable
+    {
+        yield [[]];
+        yield [['foo' => null]];
+        yield [['foo' => '']];
+        yield [['foo' => []]];
+        yield [['foo' => false]];
+        yield [['foo' => 0]];
+        yield [['foo' => '0']];
+    }
+
+    /**
+     * @test
+     * @dataProvider emptyTrueCase
+     */
+    public function shouldReturnTrueWhenEmptyTrueCaseUsingEmpty($arr): void
+    {
+        $actual = empty($arr['foo']);
+
+        $this->assertTrue($actual);
+    }
+
+    /**
+     * @test
+     * @dataProvider emptyTrueCase
+     */
+    public function shouldReturnTrueWhenEmptyTrueCaseUsingNotIssetOrEmpty($arr): void
+    {
+        $actual = empty($arr['foo']);
+
+        $this->assertTrue($actual);
+    }
+
+    /**
+     * @test
+     * @dataProvider emptyTrueCase
+     */
+    public function shouldReturnTrueWhenIssetFalseArrayCaseUsingNotIssetOrEmpty($arr): void
+    {
+        $actual = !isset($arr['foo']) || empty($arr['foo']);
+
+        $this->assertTrue($actual);
+    }
+
+    /**
+     * @test
+     * @dataProvider emptyFalseCase
+     */
+    public function shouldReturnFalseWhenEmptyFalseCaseUsingEmpty($arr): void
+    {
+        $actual = empty($arr['foo']);
+
+        $this->assertFalse($actual);
+    }
+
+    /**
+     * @test
+     * @dataProvider emptyFalseCase
+     */
+    public function shouldReturnTrueWhenEmptyFalseCaseUsingNotIssetOrEmpty($arr): void
+    {
+        $actual = !isset($arr['foo']) || empty($arr['foo']);
 
         $this->assertFalse($actual);
     }
